6dc4d37f1e96d3fb5e10f89c5c8b783500e751f6
Hawkeye <40380233+H874589148@users.noreply.github.com>
2019-02-11 22:27:08 +0800

    hello

---
+++ 12/12.2.10滚动条.py
@@ -0,0 +1,14 @@
+from tkinter import *
+root = Tk()
+root.title("Hawkeye")
+# 设定一个列表组件
+l = Listbox(root, height=6, width=15)
+# 定义滚动条，并绑定一个回调
+scroll = Scrollbar(root, command=l.yview)
+l.configure(yscrollcommand=scroll.set)
+l.pack(side=LEFT)
+scroll.pack(side=RIGHT, fill=Y)
+for item in ['apple', 'orange', 'peach', 'banana', 'melon']:
+#for item in range(20):
+    l.insert(END, item)
+root.mainloop()
